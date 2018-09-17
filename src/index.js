ecc92fea22fa2d369b8ce80e81118f3e1f4c9740
nxsyed <nxsyed.ahmed@gmail.com>
2018-09-17 13:32:34 -0700

    base template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ChatEngineCore from 'chat-engine';
 
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -9,27 +8,8 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Input from '@material-ui/core/Input';
-import opengraph from 'chat-engine-open-graph';
-
-
-const now = new Date().getTime();
-const username = ['user', now].join('-');
-
-const apiKey = "5b9afbf1719dea2000a02376";
-
-const ChatClient = ChatEngineCore.create({
-    publishKey: 'pub-c-3f89be1a-7cca-4307-8884-80b5b4855b23',
-    subscribeKey: 'sub-c-83c785b0-b219-11e8-acd6-a622109c830d'
-}, {
-    globalChannel: 'chatting'
-});
-
-ChatClient.connect(username, {
-  signedOnTime: now
-}, 'auth-key');
 
 const styles = {
   card: {
@@ -50,25 +30,10 @@ const styles = {
   },
 };
 
-class Message extends Component{
-
-  render () {
-      return ( 
-        <div > { this.props.uuid }: { this.props.text } 
-        </div>
-      );
-  }
-};
-
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.chat = new ChatClient.Chat(`openGraph`);
-    this.chat.plugin(opengraph({
-      api: (url) => `https://opengraph.io/api/1.1/site/${encodeURI(url)}?app_id=${apiKey}`,
-    }));
-
     this.state = {
       messages: [],
       chatInput: '' 
@@ -77,10 +42,6 @@ class App extends Component {
 
   sendChat = () => {
     if (this.state.chatInput) {
-        this.chat.emit('message', {
-            text: this.state.chatInput,
-            uuid: username
-        });
         this.setState({ chatInput: '' })
     }
 
@@ -88,34 +49,6 @@ class App extends Component {
 
   setChatInput = (event) => {
     this.setState({ chatInput: event.target.value })
-  }
-
-  componentDidMount() {
-    this.chat.on('message', (payload) => {
-        const { data } = payload;
-        console.log(data);
-        
-        let messages = this.state.messages;
-        if(data.img != null){
-          messages.push(
-            <div key={this.state.messages.length} style={{border: "1px solid grey"}}> { data.uuid }:
-              <img src={data.img} alt={data.title}/>
-              <h3> { data.title } </h3>
-              <a href={ data.url }> link </a>
-              <p> { data.desc } </p>
-            </div>
-          );
-        }else{
-          messages.push(
-            <Message key={ this.state.messages.length } uuid={ payload.data.uuid } text={ payload.data.text }/>
-          );
-        }
-
-        this.setState({
-            messages: messages
-        });
-
-    });
   }
 
   handleKeyPress = (e) => {
@@ -136,7 +69,7 @@ class App extends Component {
                 <List component="nav">
                   <ListItem>
                   <Typography component="div">
-                    { this.state.messages }
+                    Messages will go here
                   </Typography>
                   </ListItem>
                 </List>
@@ -153,12 +86,6 @@ class App extends Component {
                 'aria-label': 'Description',
               }}
             />
-            <Button size="small" color="primary">
-              Github
-            </Button>
-            <Button size="small" color="primary">
-              Article
-            </Button>
           </CardActions>
         </Card>
       );
@@ -167,6 +94,4 @@ class App extends Component {
 
 const ChatComponent = withStyles(styles)(App);
 
-ChatClient.on('$.ready', () => {
-    ReactDOM.render(<ChatComponent />, document.getElementById('root'));
-});
+ReactDOM.render(<ChatComponent />, document.getElementById('root'));
